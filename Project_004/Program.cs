8b4c6d8179f556a273b5d504c2ab94ff77170226
Anastasia <n.st.yy@yandex.ru>
2022-08-24 17:04:50 +0300

    Добавили папку Project_004

---
+++ Project_004/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая на вход принимает число (N),
+// а на выходе показывает все четные числа от 1 до N.
+// 5 -> 2,4
+// 8 -> 2,4,6,8
+
+Console.Write("Введите первое число: ");
+int n = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int m = int.Parse(Console.ReadLine());
+for (int i = n; i <= m; i++)
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i + " ");
+    }
+}
+Console.ReadKey();        
