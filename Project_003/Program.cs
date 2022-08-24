7c5146fcff4bd7fb126c1c1006b3a14c809201e2
Anastasia <n.st.yy@yandex.ru>
2022-08-24 15:06:40 +0300

    Добавили папку Project_003

---
+++ Project_003/Program.cs
@@ -0,0 +1,21 @@
+﻿// Напишите программу, которая на вход принимает число и выдает,
+// является ли число четным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+double a;
+string input;
+
+Console.Write("Введите число: ");
+a = Convert.ToInt32(Console.ReadLine());
+
+if (a % 2 == 0)
+{
+    Console.WriteLine("Четное число");
+}
+else
+{
+    Console.WriteLine("Нечетное число");
+}
+Console.ReadKey();
