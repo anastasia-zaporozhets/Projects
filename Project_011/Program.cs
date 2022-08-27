09940335a56453cfbc013f0fe38c2ed2cb5aa0ee
Anastasia <n.st.yy@yandex.ru>
2022-08-27 19:36:18 +0300

    Добавили папку Project_011

---
+++ Project_011/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход число (N) и помещает в массив таблицу кубов чисел от 1 до N. Полученный массив вывести на экран.
+// 3 -> [1, 8, 27]
+// 5 -> [1, 8, 27, 64, 125]
+
+int number = ReadInt("Введите число N: ");
+
+for (int i = 1; i <= number; i++)
+{
+    Console.Write($"{i * i * i}");
+}
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}    
