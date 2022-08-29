2bbab87cb5c30fc1ad7588b4253057c6ffeeb666
Anastasia <n.st.yy@yandex.ru>
2022-08-29 23:58:28 +0300

    Добавили папку Project_014

---
+++ Project_014/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int lenArray = ReadInt("Введите длину массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+    randomArray[i] = new Random().Next(1,9);
+    Console.WriteLine(randomArray[i] + " ");
+}
+
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}    
